ef355e25ddfdb154b8140e378c6bfba6b6b0b5e6
DrAkshitaChanchlani <akshita.chanchlani@mitwpu.edu.in>
2025-11-29 10:49:57 +0530

    added button and label. Added buttonTapped function

--- GitPractice/ViewController.swift
+++ GitPractice/ViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
+        
+        
         print("First Commit")
         var str = "Learning Git"
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        myLabel.text = "Hello! Everyone!!"
+    }
 }
 
