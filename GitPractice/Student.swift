5b9209c9f7fbb5e2b0904e1ab2824fa0a7a17039
DrAkshitaChanchlani <akshita.chanchlani@mitwpu.edu.in>
2025-11-29 11:19:54 +0530

    updated marks attribute in student array (learning staging-step3)

--- GitPractice/Student.swift
+++ GitPractice/Student.swift
@@ -11,4 +11,4 @@ struct Student {
     var marks: Int
 }
 
-var students: [Student] = [Student(id: 1, name: "St1"), Student(id: 2, name: "St2")]
+var students: [Student] = [Student(id: 1, name: "St1",marks: 90), Student(id: 2, name: "St2", marks:95)]
